40b1cafa655f614683882e5dad1b904fac578799
City of Vernonia GIS <gis@vernonia-or.gov>
2022-12-20 09:00:48 -0800

    remove png

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const taxMapUrl = 'https://gis.columbiacountymaps.com/TaxMaps/';
 let taxMaps;
 
 // convert pdf to tiff into `tiff` directory
-const pdf2Images = taxMap => {
+const pdf2Images = async (taxMap) => {
   execFileSync(`gswin64c`, [
     `-sOutputFile=tax-maps/tiff/${taxMap}.tiff`,
     `-sDEVICE=tiff24nc`,
     `-sCompression=lzw`,
     `-r600`,
-    `tax-maps/pdf/${taxMap}.pdf`
+    `tax-maps/pdf/${taxMap}.pdf`,
   ]);
 
   // convert pdf to jpg into `jpg` directory
@@ -24,29 +24,17 @@ const pdf2Images = taxMap => {
     `-sOutputFile=tax-maps/jpg/${taxMap}.jpg`,
     `-sDEVICE=jpeg`,
     `-r600`,
-    `tax-maps/pdf/${taxMap}.pdf`
+    `tax-maps/pdf/${taxMap}.pdf`,
   ]);
 
-  // convert pdf to png into `png` directory
-  execFileSync(`gswin64c`, [
-    `-sOutputFile=tax-maps/png/${taxMap}.png`,
-    `-sDEVICE=pngalpha`,
-    `-r600`,
-    `tax-maps/pdf/${taxMap}.pdf`
-  ]);
-  console.log(
-    chalk.green(`${taxMap}.pdf successfully converted.`)
-  );
+  console.log(chalk.green(`${taxMap}.pdf successfully converted.`));
 };
 
 // read list of tax maps
 fs.readFile('tax-maps/tax_maps.txt', 'utf-8', (readError, readData) => {
   //handle read error
   if (readError) {
-    console.log(
-      chalk.red('Failed to read tax map list.'),
-      readError
-    );
+    console.log(chalk.red('Failed to read tax map list.'), readError);
     return;
   }
 
@@ -54,27 +42,30 @@ fs.readFile('tax-maps/tax_maps.txt', 'utf-8', (readError, readData) => {
   taxMaps = readData.split('\r\n');
 
   // download tax maps and write to `pdf`, `tiff` or `jpg` directory based on file type
-  taxMaps.forEach(taxMap => {
+  taxMaps.forEach((taxMap) => {
     // download file
     download(`${taxMapUrl}${taxMap}.pdf`)
-      .then(async downloadData => {
+      .then(async (downloadData) => {
         // write pdf
-        fs.writeFile(`tax-maps/pdf/${taxMap}.pdf`, downloadData, pdfWriteError => {
+        fs.writeFile(
+          `tax-maps/pdf/${taxMap}.pdf`,
+          downloadData,
+          (pdfWriteError) => {
+            // handle write error
+            if (pdfWriteError) {
+              console.log(
+                chalk.red(`Failed to write ${taxMap}.pdf.`),
+                pdfWriteError
+              );
+              return;
+            }
 
-          // handle write error
-          if (pdfWriteError) {
-            console.log(
-              chalk.red(`Failed to write ${taxMap}.pdf.`),
-              pdfWriteError
-            );
-            return;
+            // convert to tiff and jpg
+            pdf2Images(taxMap);
           }
-
-          // convert to tiff and jpg
-          pdf2Images(taxMap);
-        });
+        );
       })
-      .catch(downloadError => {
+      .catch((downloadError) => {
         // handle download error
         console.log(
           chalk.red(`Failed to download ${taxMap} PDF.`),
